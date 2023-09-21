47e2dabbbd688d2269dd69a2e25c4c0960b3a972
Dipu Haider <dipuhaider@gmail.com>
2023-09-21 12:06:49 +0600

    install react icons & toggle icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { TfiAlignJustify, TfiClose } from "react-icons/tfi";
 
 const NavBar = () => {
 
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, path: '/home', name: 'Home' },
         { id: 2, path: '/about', name: 'About' },
@@ -12,6 +15,16 @@ const NavBar = () => {
 
     return (
         <nav>
+            <div  className="md:hidden" onClick={()=> setOpen(!open)}>
+                {
+                    open === true ? 
+                    <TfiClose className="text-4xl md:hidden"></TfiClose> 
+                    : 
+                    <TfiAlignJustify className="text-4xl md:hidden"></TfiAlignJustify>
+                }
+            </div>
+            
+            
             <ul className="md:flex">
                 {
                     routes.map( route => <Link key={route.id} route={route}></Link> )
