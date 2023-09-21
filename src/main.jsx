d2d5ec2afd90ea8c7664e33cf709140fc76b1794
Dipu Haider <dipuhaider@gmail.com>
2023-09-21 15:08:36 +0600

    route in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>,
+      },
+      {
+        path: '/contact',
+        element:<Contact></Contact>,
+      },
+    ]
+  },
+  {
+    path: 'header',
+    element: <Header></Header>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
