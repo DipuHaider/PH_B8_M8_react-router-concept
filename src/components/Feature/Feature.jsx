c2c237dd71eb2ad267ebf1ae946216b916009fe8
Dipu Haider <dipuhaider@gmail.com>
2023-09-21 14:42:16 +0600

    finalizing priceoption

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Feature = ({feature}) => {
+    return (
+        <div>
+            <h2>{feature}</h2>
+        </div>
+    );
+};
+
+Feature.propTypes = {
+    feature: PropTypes.string
+}
+
+export default Feature;
--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types';
+import Feature from '../Feature/Feature';
 
 const PriceOption = ({option}) => {
+    const {name, price, features} = option;
+
     return (
         <div>
-            <h2>{option.name}{option.price}</h2>
+            {/* <h2>{option.name}{option.price}</h2> //before destruturing*/}
+            <h2>{name} {price}</h2>
+            {
+                features.map((feature ,idx)=> <Feature key={idx} feature={feature}></Feature>)
+            }
         </div>
     );
 };
