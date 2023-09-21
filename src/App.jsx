73a69905adc9227bfb7fc66e6b8a163f1fa955da
Dipu Haider <dipuhaider@gmail.com>
2023-09-21 11:54:53 +0600

    Use daisyUi Nav & install react proptype

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
   return (
     <>
+    {/* <DaisyNav></DaisyNav> */}
+    <NavBar></NavBar>
       <h1 className='text-lg text-blue-300'>React router concept</h1>
     </>
   )
