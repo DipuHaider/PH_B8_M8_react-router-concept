7b1a0551d3a674996b6b335c498304f91802a585
Dipu Haider <dipuhaider@gmail.com>
2023-09-22 15:55:16 +0600

    useNavigate & Go Back Button

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const User = ({user}) => {
+const User = ({ user }) => {
 
-    const {id, name, email, phone} = user;
+    const { id, name, email, phone } = user;
+
+    const navigate = useNavigate();
+
     const userStyle = {
         border: '2px solid yellow',
         padding: '10px',
         borderRadius: '20px'
+    }
+
+    const handleShowUser = () => {
+        navigate(`/user/${id}`);
     }
 
     return (
@@ -17,6 +24,8 @@ const User = ({user}) => {
             <h3>{email}</h3>
             <h3>{phone}</h3>
             <Link to={`/user/${id}`}>Show Details</Link>
+            <Link to={`/user/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleShowUser}>Click to see details</button>
         </div>
     );
 };
--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,13 +1,21 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
     const user = useLoaderData();
+
+    const navigate = useNavigate();
+
     const{name, website} = user;
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
 
     return (
         <div>
             <h2>details about user:{name} </h2>
             <h2>Web: {website} </h2>
+            <button onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
