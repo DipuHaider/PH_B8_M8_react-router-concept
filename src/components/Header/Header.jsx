4aa39623ec928ee2e13d645973a40b06729175e8
Dipu Haider <dipuhaider@gmail.com>
2023-09-24 12:41:19 +0600

    use navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
-import './Header.css'
+// import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <h1>navbar</h1>
             <nav>
-                <Link to="/">Home</Link>
+                {/*<Link to="/">Home</Link>
                 <Link to="/users">Users</Link>
                 <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact">Contact</Link> */}
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
             </nav>
         </div>
     );
